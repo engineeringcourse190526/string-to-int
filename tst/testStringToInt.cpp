81fcc707bd608d75584d6bc6b21ccd28e51dd20d
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-05-11 18:48:53 +0200

    :tada: init commit

---
+++ tst/testStringToInt.cpp
@@ -0,0 +1,16 @@
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(StringToInt){
+
+    void setup() {
+    }
+
+    void teardown() {
+
+    }
+};
+
+TEST(StringToInt, test) {
+    FAIL("Start from here...")
+}
+
